806b577c28c6548c1073b27b0503bc0a9376196d
Sadia Imran <164052169+codewithsadia@users.noreply.github.com>
2024-12-13 08:39:22 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,28 +1,29 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
-const Cart = () => {
-  return (
-    <div className='grid grid-cols-2 gap-4 mt-6'>
-    <div className="p-2">
-      <Image
-        src="/images/i1.png"
-        alt='Image i1'
-        width={1000}
-        height={1000}
-        layout="responsive"
-      />
-    </div>
-    <div className="p-2">
-      <Image
-        src="/images/i2.png"
-        alt='Image i2'
-        width={500}
-        height={500}
-        layout="responsive"
-      />
-    </div>
-  </div>
-  )
-}
 
-export default Cart;
+const Detail = () => {
+  return (
+    <div className='grid grid-cols-2 gap-4'>
+      <div className='p-2'>
+        <Image
+          src="/images/shoesp1.png"
+          alt='shoes'
+          layout="responsive"
+          width={500} // Used for aspect ratio
+          height={500} // Used for aspect ratio
+        />
+      </div>
+      <div className='p-2'>
+        <Image
+          src="/images/text1.png"
+          alt='shoes description'
+          layout="responsive"
+          width={500} // Used for aspect ratio
+          height={500} // Used for aspect ratio
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Detail;
